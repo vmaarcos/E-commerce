b21b0d98ac55e45902a0c9064aa699586d6963b8
JoséW <124584573+Jossxz@users.noreply.github.com>
2023-10-12 14:50:17 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import TestApi from './Comp/TestApi'
+import TestApi2 from './Comp/TestApi2'
+import TestApi3 from './Comp/TestApi3'
+
+function App() {
+  return (
+    <main>
+        <TestApi/>
+        <TestApi2/>
+        <TestApi3/>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/Comp/TestApi.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+
+function ProductDetails() {
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products/1')
+      .then((response) => response.json())
+      .then((data) => setProduct(data))
+      .catch((error) => console.error('Erro ao buscar dados:', error));
+  }, []);
+
+  return (
+    <div>
+      {product ? (
+        <div>
+          <h1>{product.title}</h1>
+          <p>{product.description}</p>
+          <img src={product.image} alt='img1'></img>
+          <p>Preço: {product.price}</p>
+        </div>  
+      ) : (
+        <p>Carregando...</p>
+      )}
+    </div>
+  );
+}
+
+export default ProductDetails;

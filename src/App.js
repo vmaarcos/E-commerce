14ef95a6248576a25b80fbd45e82ff8a92fef422
vmaarcos <111014095+vmaarcos@users.noreply.github.com>
2023-10-13 07:53:38 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,8 +1,54 @@
+import React,{useState, useEffect} from 'react';
+import './App.css';
 
-function App() {
-  return (
-  <h1>teste</h1>
-  );
+const App=()=>{
+  const [fake, setFake]=useState([]);
+  console.log(fake);
+  useEffect(()=>{
+    fakeStore();
+  }, [])
+  
+
+    const fakeStore=async()=>{
+      const response=await fetch("https://fakestoreapi.com/products");
+     // console.log(response);
+      const jsonData=await response.json();
+      // console.log(jsonData);
+      setFake(jsonData);
+    }
+
+    //fakeStore();
+
+  return(
+    <>
+    <h2>Fake Api</h2>
+    <div className='container'>
+      {fake.map((values)=>{
+        return(
+          <>
+          <div className='box'>
+        <div className='content'>
+          <h5>{values.title}</h5>
+          <p>{values.descrição}</p>
+        </div>
+        <img src={values.image} alt=''/>
+      </div>
+          </>
+        )
+      }
+      )}
+      <div className='box'>
+        <div className='content'>
+          <h5>title</h5>
+          <p>descrição do bglh xará</p>
+        </div>
+        <h1>image</h1>
+      </div>
+      
+       
+    </div>
+    </>
+  )
 }
 
 export default App;

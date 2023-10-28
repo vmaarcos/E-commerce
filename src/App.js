0332861c1d1cce60727fc2f95667fbe3668f818d
JoséW <124584573+Jossxz@users.noreply.github.com>
2023-10-28 18:55:54 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React,{useState, useEffect} from 'react';
 import './App.css';
+import IconCar from './image/carrinho-de-compras.png';
 
 const App=()=>{
   const [fake, setFake]=useState([]);
@@ -21,21 +22,37 @@ const App=()=>{
 
   return(
     <>
-    <div className='containerHeader'>
-    <img src='' className='imageIcon'></img>
-    <h1>E-Commerce</h1>
+
+    <div className='ContainerHeader'>
+    <img src={IconCar} className='imageIcon'></img>
+    <h1 className='Logo'>E-Commerce</h1>
     </div>
-    <div className='container'>
+
+
+
+    <div className='ContainerBanner'>
+          <img src='' className='Banner_1'></img>
+          </div>
+
+
+    <div className='ContainerMap'>
       {fake.map((values)=>{
         return(
+
           <>
-          <div className='box'>
-        <div className='content'>
-          <h5>{values.title}</h5>
-          <p>{values.descrição}</p>
+
+        <div className='ContainerApi'>    
+        <img src={values.image} className='ImgItem'/>
+        <h5 className='Text_1'>{values.title}</h5>
+        <h6 className='Text_2'>{values.category}</h6>
+
+        <div className='ContainerNumbers'>
+        <h3 className='Number_1'>R${values.price}</h3>
+        <h3 className='Number_2'>{values.rating.rate}</h3>
+        <h3 className='Number_3'>{values.rating.count}</h3>
+          </div>
         </div>
-        <img src={values.image} alt=''/>
-      </div>
+          
           </>
         )
       }
--- src/App.jsx
+++ src/App.jsx
@@ -6,5 +6,4 @@ function App() {
     </main>
   );
 }
-
 export default App;

35115df496bf67eb83f97dc58c60ed638d70c300
vmaarcos <111014095+vmaarcos@users.noreply.github.com>
2023-11-03 08:08:04 -0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
-import Header from './Comp/Header';
+import Header from './pages/Header';
 
 ReactDOM.render(
   <Router>
